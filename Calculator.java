10ae4215b657adc4825d539a3863f214bc80ec1f
nguyenquochuy174 <huymonkey6403@gmail.com>
2025-06-29 17:45:11 +0700

    Add file Calculator.java

---
+++ Calculator.java
@@ -0,0 +1,12 @@
+public class Calculator {
+    enum SIGN{
+        minus, plus, divide, multiply
+    }
+    private Double calculator(SIGN sign, Double arg1, Double arg2) {
+        switch (sign) {
+            default:
+                break;
+        }
+        return 0d;
+    }
+}
